25435c4247fa6f4a0c054742419f3649de6a9eda
Andrii Rodionov <andrii.rodionov@gmail.com>
2014-11-04 15:06:13 +0200

    Added script to delete user, jobs and team

---
+++ HudsonDeleteUserJobsAndTeam.groovy
@@ -0,0 +1,10 @@
+﻿import hudson.model.*
+import org.eclipse.hudson.security.team.*
+
+def userName = "test"
+
+def hudsonInstance = Hudson.getInstance()
+hudsonInstance.items.findAll{ job -> job.name.startsWith("T" + userName + ".") }.each{ job -> job.delete() }
+hudsonInstance.getTeamManager().deleteTeam("T" + userName)
+hudsonInstance.getUser(userName).delete()
+hudsonInstance.deleteView(hudsonInstance.getView(userName))
